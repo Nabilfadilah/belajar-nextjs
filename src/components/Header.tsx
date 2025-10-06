0bcd2ca59ff89428b1d3d468f59affa7bbc2f610
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-10-06 11:23:14 +0700

    Menambah Next Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="header">
       <nav>
         <div className="logo">
-          <a href="">Logo</a>
+          <Link href="/">Logo</Link>
+        </div>
+        <div className="nav-links">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
         </div>
       </nav>
     </header>
