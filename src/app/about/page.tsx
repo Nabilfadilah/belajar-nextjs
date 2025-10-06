ad48c09b2942a95602b0cd1f4e5917f67bee1598
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-10-06 11:17:52 +0700

    Membuat page about routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,32 @@
+const page = () => {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex
+        expedita qui nihil, eos reprehenderit, quidem vitae recusandae
+        exercitationem, similique illum quod facere sit modi odio cum dicta
+        doloremque. Officia possimus quos animi voluptatibus? A eligendi
+        laboriosam illum quo deserunt voluptates, laudantium libero, sit quasi
+        ea voluptas quia eaque doloribus quidem? Modi iste repudiandae cumque
+        corrupti ullam. Consequuntur dolore iure nihil et rem reprehenderit
+        magni dignissimos! Fugit omnis illum quas sapiente molestiae voluptate
+        ipsam sint at, esse saepe neque illo non ut quibusdam labore iure vero
+        qui autem, iste hic consequatur. Neque necessitatibus enim maxime veniam
+        omnis ad nostrum quisquam harum officiis reiciendis voluptatibus nam
+        vitae fuga saepe deserunt cumque, nisi architecto, sequi explicabo. Ut
+        itaque architecto voluptas assumenda et perspiciatis eos at est
+        explicabo blanditiis esse amet, animi fugiat veritatis quas deleniti
+        reiciendis dolor aut dolore culpa nulla tempore voluptatem! Aperiam
+        aliquam alias, nemo eaque aut cum adipisci, autem placeat iusto laborum
+        delectus nostrum est sit! Neque consequuntur sapiente sed animi harum
+        minus officia, eum ducimus ipsam voluptates nisi ea hic nihil
+        perferendis unde! Dolores ab nulla labore consequatur voluptatibus ad
+        aliquid numquam omnis vel nesciunt a esse ullam amet nisi voluptatum
+        sunt voluptate neque, id possimus officiis. Est.
+      </p>
+    </>
+  );
+};
+
+export default page;
