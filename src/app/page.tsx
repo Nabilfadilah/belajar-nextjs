d600e2991c3d1650be58b2f118067bed90631ae4
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-10-07 16:14:45 +0700

    Mengambil data dari API public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,38 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+async function getLastPost() {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=5"
+  );
+  const posts = await res.json();
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getLastPost();
+
   return (
-    <div className="main-heading">
-      <h1>Selamat Datang di NextJS News</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in modi
-        vitae dolor. Pariatur perferendis quos quam similique asperiores. Nulla
-        maxime fugit id distinctio libero tenetur sit commodi modi nihil.
-      </p>
-    </div>
+    <>
+      <div className="main-heading">
+        <h1>Selamat Datang di NextJS News</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in
+          modi vitae dolor. Pariatur perferendis quos quam similique asperiores.
+          Nulla maxime fugit id distinctio libero tenetur sit commodi modi
+          nihil.
+        </p>
+      </div>
+
+      <div className="post-list">
+        {posts.map((post: any) => (
+          <div className="post-item" key={post.id}>
+            <h2>
+              <Link href="/">{post.title}</Link>
+            </h2>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
